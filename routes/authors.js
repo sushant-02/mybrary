dcfb3960db795505a29bb7346f9bdafdd60fceef
Sushant <skp000002@gmail.com>
2022-01-16 19:55:53 +0530

    Initial author routes

---
+++ routes/authors.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+
+// Schema
+const Author = require("../models/author");
+
+// All authors route
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
+  }
+
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render("authors/index", {
+      authors: authors,
+      searchOptions: req.query,
+    });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+});
+
+// New author route
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
+
+// Create author route
+router.post("/", async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+  try {
+    const newAuthor = await author.save();
+    // res.redirect(`authors/${newAuthor.id}`);
+    res.redirect("authors");
+  } catch (err) {
+    console.log(err);
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error creating Author",
+    });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const app = express();
 
 mongoose
@@ -12,7 +14,9 @@ mongoose
   .then(() => console.log("Connected to mongodb"))
   .catch((error) => console.log(error));
 
+// Routes
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -20,6 +24,9 @@ app.set("layout", "layouts/layout");
 
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
